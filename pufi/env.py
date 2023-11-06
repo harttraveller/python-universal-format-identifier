09958e6f12ecfce45708343511b43f18ba76e594
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-11-05 23:43:30 -0500

    pufi/env: update

--- pufi/env.py
+++ pufi/env.py
@@ -1,6 +1,11 @@
-TEXT_FORMATS = {
-    "html",
+from pydantic import BaseModel, field_validator
+
+FORMATS = {
     "txt",
+}
+
+FORMATS_TODO = {
+    "html",
     "json",
     "csv",
     "xml",
@@ -25,3 +30,19 @@ TEXT_FORMATS = {
     "srt",
     "vtt",
 }
+
+
+class DataFormat(BaseModel):
+    name: str
+
+    @field_validator("name")
+    def __validate_name(cls, name: str) -> str:
+        if name not in FORMATS:
+            raise ValueError("unknown file format")
+        return name
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return self.name
