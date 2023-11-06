9db3c1f998d2fb6e86665089a01fd8ed7a75998f
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-11-05 23:59:06 -0500

    pufi/core: move code

--- pufi/core.py
+++ pufi/core.py
@@ -1,8 +1,23 @@
 from pathlib import Path
 from typing import Optional, Union
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+from pufi.env import FORMATS
 
-from pufi.env import DataFormat
+
+class DataFormat(BaseModel):
+    name: str
+
+    @field_validator("name")
+    def __validate_name(cls, name: str) -> str:
+        if name not in FORMATS:
+            raise ValueError("unknown file format")
+        return name
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return self.name
 
 
 class ResolutionResult(BaseModel):
