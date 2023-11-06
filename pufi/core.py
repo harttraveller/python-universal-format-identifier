1cbeba8f72aa0b6cb81f999c35f7019e0b6b50f8
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-11-05 23:47:13 -0500

    pufi/core: update params

--- pufi/core.py
+++ pufi/core.py
@@ -5,18 +5,18 @@ from pufi.env import DataFormat
 
 
 def resolve(
-    text: Optional[str] = None,
+    raw: Optional[str] = None,
     bin: Optional[bytes] = None,
-    path: Optional[Union[str, Path]] = None,
+    loc: Optional[Union[str, Path]] = None,
     uri: Optional[str] = None,
-    check: bool = False,
+    val: bool = False,
 ) -> DataFormat:
-    if check:
+    if val:
         raise NotImplementedError(
             "Reading in the resource to check it is not yet supported, "
             "you should read it in and pass the text or binary data to this function."
         )
     # check if appropriate params have been passed in
-    if all([text is None, bin is None, path is None, uri is None]):
+    if all([raw is None, bin is None, loc is None, uri is None]):
         raise ValueError("You must pass an argument to at least one of the parameters.")
     # assess if the path or uri
