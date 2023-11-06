7e61f4fda237c6e5831b7cc654cf39be1487cd40
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-11-05 23:34:31 -0500

    pufi/core: add check

--- pufi/core.py
+++ pufi/core.py
@@ -14,3 +14,7 @@ def resolve(
             "Reading in the resource to check it is not yet supported, "
             "you should read it in and pass the text or binary data to this function."
         )
+    # check if appropriate params have been passed in
+    if all([text is None, bin is None, path is None, uri is None]):
+        raise ValueError("You must pass an argument to at least one of the parameters.")
+    # assess if the path or uri
