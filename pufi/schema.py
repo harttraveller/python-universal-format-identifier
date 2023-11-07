6bfbca8504f88e425ccdc056d64f183ee0a9ace5
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-11-07 11:28:37 -0500

    pufi/schema: comment temp broken code

--- pufi/schema.py
+++ pufi/schema.py
@@ -1,15 +1,16 @@
 from pydantic import BaseModel, field_validator
-from pufi.env import FORMATS
+
+# from pufi.env import FORMATS
 
 
 class DataFormat(BaseModel):
     name: str
 
-    @field_validator("name")
-    def __validate_name(cls, name: str) -> str:
-        if name not in FORMATS:
-            raise ValueError("unknown file format")
-        return name
+    # @field_validator("name")
+    # def __validate_name(cls, name: str) -> str:
+    #     if name not in FORMATS:
+    #         raise ValueError("unknown file format")
+    #     return name
 
     def __str__(self) -> str:
         return self.name
