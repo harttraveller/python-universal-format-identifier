7b187b014983bd6747e05b3138171ff080e9bca1
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-11-07 11:22:46 -0500

    pufi/__init__: upd

--- pufi/__init__.py
+++ pufi/__init__.py
@@ -1,19 +1,1 @@
-from urllib.request import urlretrieve
-from pufi.variable import (
-    CACHE,
-    CATEGORIES_LOCAL,
-    CATEGORIES_URL,
-    EXTENSIONS_LOCAL,
-    EXTENSIONS_URL,
-)
-
-
-# ensure that package resource files are available
-if not CACHE.exists():
-    CACHE.mkdir()
-if not CATEGORIES_LOCAL.exists():
-    urlretrieve(CATEGORIES_URL, CATEGORIES_LOCAL)
-if not EXTENSIONS_LOCAL.exists():
-    urlretrieve(EXTENSIONS_URL, EXTENSIONS_LOCAL)
-
 from pufi.core import resolve
