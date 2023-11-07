ed64be957bce9027838b848308514083942291f8
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-11-07 11:13:07 -0500

    pufi/__init__: add cat retrieval

--- pufi/__init__.py
+++ pufi/__init__.py
@@ -3,5 +3,5 @@ from pufi.env import CACHE, LOC_CAT, LOC_EXT, WEB_CAT, WEB_EXT
 
 
 CACHE.mkdir(exist_ok=True)
-# if not LOC_CAT.exists():
-#     urlretrieve()
+if not LOC_CAT.exists():
+    urlretrieve(WEB_CAT, LOC_CAT)
