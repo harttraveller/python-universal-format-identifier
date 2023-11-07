2214ad6ed08e9cc7c150022c72d56a94b86dc5aa
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-11-07 11:12:45 -0500

    pufi/__init__: upd env vars

--- pufi/__init__.py
+++ pufi/__init__.py
@@ -1,5 +1,7 @@
 from urllib.request import urlretrieve
-from pufi.env import CACHE
+from pufi.env import CACHE, LOC_CAT, LOC_EXT, WEB_CAT, WEB_EXT
 
 
 CACHE.mkdir(exist_ok=True)
+# if not LOC_CAT.exists():
+#     urlretrieve()
