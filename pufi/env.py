b78274a42f6c9d41b22e14beea680eed3f1ff966
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-11-07 11:11:15 -0500

    pufi/env: .

--- pufi/env.py
+++ pufi/env.py
@@ -3,3 +3,8 @@ from pathlib import Path
 
 HOME = Path.home()
 CACHE = HOME / ".pufi"
+
+LOC_CAT = CACHE / "categories.json"
+LOC_EXT = CACHE / "extensions.json"
+URL_CAT = "https://github.com/dyne/file-extension-list/raw/master/pub/categories.json"
+URL_EXT = "https://github.com/dyne/file-extension-list/raw/master/pub/extensions.json"
