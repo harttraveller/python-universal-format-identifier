d55bdad09d479bfbdbe3ea8f8730393d84059f0b
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-11-07 11:22:40 -0500

    pufi/env: create

---
+++ pufi/env.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from urllib.request import urlretrieve
+
+
+HOME = Path.home()
+CACHE = HOME / ".pufi"
+
+CATEGORIES_LOCAL = CACHE / "categories.json"
+EXTENSIONS_LOCAL = CACHE / "extensions.json"
+CATEGORIES_URL = (
+    "https://github.com/dyne/file-extension-list/raw/master/pub/categories.json"
+)
+EXTENSIONS_URL = (
+    "https://github.com/dyne/file-extension-list/raw/master/pub/extensions.json"
+)
+
+# ensure that package resource files are available
+if not CACHE.exists():
+    CACHE.mkdir()
+if not CATEGORIES_LOCAL.exists():
+    urlretrieve(CATEGORIES_URL, CATEGORIES_LOCAL)
+if not EXTENSIONS_LOCAL.exists():
+    urlretrieve(EXTENSIONS_URL, EXTENSIONS_LOCAL)
