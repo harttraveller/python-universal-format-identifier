1f251f1d957ade4c2fb3c6040d5fab5bd0fba226
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-11-07 11:20:56 -0500

    pufi: reorg

---
+++ pufi/__init__.py
@@ -0,0 +1,18 @@
+from urllib.request import urlretrieve
+from pufi.variable import (
+    CACHE,
+    CATEGORIES_LOCAL,
+    CATEGORIES_URL,
+    EXTENSIONS_LOCAL,
+    EXTENSIONS_URL,
+)
+
+
+if not CACHE.exists():
+    CACHE.mkdir()
+if not CATEGORIES_LOCAL.exists():
+    urlretrieve(CATEGORIES_URL, CATEGORIES_LOCAL)
+if not EXTENSIONS_LOCAL.exists():
+    urlretrieve(EXTENSIONS_URL, EXTENSIONS_LOCAL)
+
+from pufi.core import resolve
